a51e5ced598d15bb026a269f4ecf83bc174fd1d9
Zach Taylor <zach.taylor673@gmail.com>
2018-02-19 21:29:36 -0600

    add print functionality to dotenv cmd

--- cmd/dotenv/main.go
+++ cmd/dotenv/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"ztaylor.me/env"
 )
 
@@ -9,5 +11,11 @@ func main() {
 	env.Bootstrap()
 	for k, v := range env.Cache {
 		fmt.Printf("%s=%s\n", k, v)
+	}
+	for _, line := range os.Args[1:] {
+		args := strings.Split(strings.Trim(line, `-`), "=")
+		if len(args) == 1 {
+			fmt.Printf("%s=%s\n", args[0], env.Get(args[0]))
+		}
 	}
 }
